83b7b10c6a4d7b3453533832b87948afd1c1cf03
RasmusaAndersson <rasmus.alexander.andersson@gmail.com>
2023-11-15 11:57:59 +0100

    Finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,50 +11,56 @@ import java.util.Scanner;
 
             while (true) {
                 System.out.println("Enter a number:");
+                while (!input.hasNextDouble()) {
+                    System.out.println("Invalid number, re-enter:");
+                    input.next();
+                }
                 num1 = input.nextDouble();
 
                 System.out.println("Enter an operator (+, -, *, /): ");
                 operator = input.next().charAt(0);
-
-                System.out.println("Enter a number:");
-                num2 = input.nextDouble();
-
                 while (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
                     System.out.println("Invalid operator. Please enter (+, -, *, /): ");
                     operator = input.next().charAt(0);
                 }
+                    System.out.println("Enter a number:");
+                    while (!input.hasNextDouble()) {
+                        System.out.println("Invalid number, re-enter:");
+                        input.next();
+                    }
+                    num2 = input.nextDouble();
 
 
-                switch (operator) {
-                    case '+':
-                        result = num1 + num2;
+                    switch (operator) {
+                        case '+':
+                            result = num1 + num2;
+                            break;
+
+                        case '-':
+                            result = num1 - num2;
+                            break;
+
+                        case '*':
+                            result = num1 * num2;
+                            break;
+
+                        case '/':
+                            result = num1 / num2;
+                            break;
+
+                        default:
+                            System.out.println("Invalid operator");
+                            return;
+                    }
+
+                    System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+
+                    System.out.println("Do you want to continue? (y/n)");
+                    char choice = input.next().charAt(0);
+
+                    if (choice == 'n') {
                         break;
-
-                    case '-':
-                        result = num1 - num2;
-                        break;
-
-                    case '*':
-                        result = num1 * num2;
-                        break;
-
-                    case '/':
-                        result = num1 / num2;
-                        break;
-
-                    default:
-                        System.out.println("Invalid operator");
-                        return;
-                }
-
-                System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
-
-                System.out.println("Do you want to continue? (y/n)");
-                char choice = input.next().charAt(0);
-
-                if (choice == 'n') {
-                    break;
+                    }
                 }
             }
         }
-    }
